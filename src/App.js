1adfb38726e0f0d362c207b91eb0829c61be6820
roberto cortes <robertcortesgrimm223@gmail.com>
2020-06-16 16:38:56 -0500

    home component created

--- src/App.js
+++ src/App.js
@@ -1,32 +1,76 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import Navbar from './Navbar.js'
+import shoppingCart from './shopping-cart.png'
+import facebookLogo from './App Logo Inspiraton 42.png'
+import instagramLogo from './Icon.png'
+import cashappLogo from './Cash App 1.png'
+import venmoLogo from './venmologo.png'
+import logo from './LOGO.png'
+import cinRoll from './Cinnamon Rolls.png'
+import glazCinDo from './Glazed Cinnamon Donuts.png'
+import twoToDo from './Two Tone Donut.png'
+
+import { Zoom } from 'react-slideshow-image'
+
+const images = [
+  cinRoll,
+  glazCinDo,
+  twoToDo
+]
+
+const zoomOutProperties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  scale: 0.4,
+  arrows: true
+}
+
 
 function App() {
   return (
 
       <React.Fragment>
-        <h1 classname='title'>Thank God 4 Raw & Vegan Treats</h1>
-        <nav classname='nav-bar'>
-          <ul nav-bar-item></ul>
-          <ul nav-bar-item></ul>
-          <ul nav-bar-item></ul>
-          <ul nav-bar-item></ul>
+        <h1 className='title'><img src={logo} alt={'image of website name, thank god for raw & vegan treats'}/></h1>
+        <nav className='nav-bar'>
+          <ul>
+            <li className='nav-bar-item'><a href='#'>Home</a></li>
+            <li className='nav-bar-item'><a href='#'>Treats</a></li>
+            <li className='nav-bar-item'><a href='#'>Photos & Reviews</a></li>
+            <li className='nav-bar-item'><a href='#'>About</a></li>
+            <li className='nav-bar-item'><a href='#'><img className='shopping-cart' src={shoppingCart} alt={'shopping cart image'}/></a></li>
+          </ul>
         </nav>
-        <img classname='home-img'/>
-        <p classname='contact'>Contact Me</p>
-        <p classname='phone'></p>
-        <p classname='email'></p>
-        <img classname='instagram'/>
-        <img classname='facebook'/>
-        <p>We Accept:</p>
-        <img classname='cashapp'/>
-        <img classname='venmo'/>
+       <div className="slide-container">
+         <Zoom {...zoomOutProperties}>
+           {
+             images.map((each, index) => <img  key={index} style={{width: "100%"}} src={each} />)
+           }
+         </Zoom>
+       </div>
+        <p className='contact'>Contact Me</p>
+        <p className='phone'>Phone: (773) 879-4208</p>
+        <p className='email'>Email: chelly67mom@yahoo.com</p>
+        <a href='https://www.instagram.com/thankgod4rawvegantreats_/' target="_blank">
+          <img className='instagram' src={instagramLogo} alt={'instagram logo'}/>
+        </a>
+        <a href='https://www.facebook.com/Michelle-Scott-CEO-197474354500232/' target="_blank">
+          <img className='facebook' src={facebookLogo} alt={'facebook logo'}/>
+        </a>
+        <p className='payment-options'>We Accept:</p>
+        <img className='cashapp' src={cashappLogo} alt={'cashapp logo'}/>
+        <img className='venmo' src={venmoLogo} alt={'venmo logo'}/>
 
-        <div classname='review-container'>
-          <ul reivew-item></ul>
-          <ul reivew-item></ul>
-          <ul reivew-item></ul>
+        <div className='review-container'>
+          <ul className='reviews'>
+            <p className='review-item'>“Michelle it was the best cake ever, we ate the whole thing!”</p>
+            <p className='review-item'>“#blackgirlmagic!”</p>
+            <p className='review-item'>“We couldnt stop eating it literally.”</p>
+          </ul>
+          
+          
         </div>
 
       </React.Fragment>
