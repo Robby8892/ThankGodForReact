75f488f084ce55cbbffe3cd9890b5965659a0025
roberto cortes <robertcortesgrimm223@gmail.com>
2020-06-17 08:47:41 -0500

    added simple login form

--- src/AdminContainer/LoginContainer/index.js
+++ src/AdminContainer/LoginContainer/index.js
@@ -10,9 +10,21 @@ export default class LoginContainer extends Component {
 
 	render(){
 		return(
-			<div>
-				LoginContainer
-			</div>
+			<React.Fragment>
+				<div id='login-container'>
+					<form id='login-form'>
+						<fieldset>
+							<legend>Login Here</legend>
+							<label for="loginName"></label>
+							<input id='loginName' type="text" name="username" placeholder="Enter your login name"/>
+							<label for="password"></label>
+							<input id='password' type="password" name="password" placeholder="Enter your password"/>
+							<button>Login</button>
+						</fieldset>
+					</form>
+				</div>
+			</React.Fragment>
+
 		)
 	}
 }
