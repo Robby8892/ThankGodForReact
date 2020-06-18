ec4bed90527e5641f36a89a505d5fa7c65c30f83
roberto cortes <robertcortesgrimm223@gmail.com>
2020-06-18 13:22:01 -0500

    added styling

--- src/NavbarContainer/CartModal/index.js
+++ src/NavbarContainer/CartModal/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {Button, Header, Modal, Segment, Label, Image } from 'semantic-ui-react'
 import axios from 'axios'
+import './index.css'
 
 export default function CartModal(props) {
 
@@ -14,20 +15,20 @@ export default function CartModal(props) {
 				)
 			};
 			return(
-				<div key={cartId}>
+				<div key={_id}>
 					<Header as='h4'>{name}</Header>
-					<p>{price}</p>
-					<Image size='small'  src={process.env.REACT_APP_API_URI + `image/treat/${_id}`}/>
-					
+					<p>${price}</p>
+					<Image className='modal-img' size='small'  src={process.env.REACT_APP_API_URI + `image/treat/${_id}`} alt={name}/>
+
 				</div>
 			)
 		})
 		return(
-			<Modal size='mini' closeIcon open={props.cartModal} onClose={props.closeCartModal}>
+			<Modal className='modal-container' size='small' closeIcon open={props.cartModal} onClose={props.closeCartModal}>
 				<Header as='h2'>{props.userCartInfo.length > 0 ? 'Here are items in your cart.' : 'Your cart is empty'}</Header>
-				{userCartInfo}
+				<Modal.Content className='content-container'>{userCartInfo}</Modal.Content>
 				{props.userCartInfo.length > 0 ? 
-					<Modal.Actions><Button>Checkout</Button><Button>Clear Cart</Button></Modal.Actions>
+					<Modal.Actions><Button color='green'>Checkout</Button><Button color='red'>Clear Cart</Button></Modal.Actions>
 				 :
 				 null
 				  }
