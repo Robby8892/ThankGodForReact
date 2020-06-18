fa73feeb24e9aea56db22cd2c24e0c191308800c
roberto cortes <robertcortesgrimm223@gmail.com>
2020-06-18 00:53:03 -0500

    create cart fetch all works

--- src/CartContainer/index.js
+++ src/CartContainer/index.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
-export default class CartContainer extends Component{
+export default class CartContainer extends Component {
 	constructor(props){
 		super()
 		this.state = {
 			cart: false,
 			cartId: ''
 		}
+	}
+
+	createCart = async () => {
+		const cartResponse = await axios.post(process.env.REACT_APP_API_URI + 'cart/new')
+			.then( res => {
+				console.log('here is a good response',res.data);
+			})
+		
+		console.log(cartResponse);
+	}
+
+	componentDidMount(){
+		this.createCart()
 	}
 
 	render(){
